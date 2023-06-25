ee5058b5bbc5662d4ab5f5b088641c6c1e3fd051
nikolayhotim1 <hotim_nikolay@mail.ru>
2023-06-25 03:07:51 +0300

    feature: authentication

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,13 @@
-// import { authOptions } from '@/lib/auth'
-// import { getServerSession } from 'next-auth'
+import { authOptions } from '@/lib/auth'
+import { getServerSession } from 'next-auth'
 import Link from 'next/link'
 import { Icons } from './Icons'
 import { buttonVariants } from './ui/Button'
 // import { UserAccountNav } from './UserAccountNav'
 // import SearchBar from './SearchBar'
 
-export default function Navbar() {
-	// const session = await getServerSession(authOptions)
+export default async function Navbar() {
+	const session = await getServerSession(authOptions)
 	return (
 		<div className='fixed top-0 inset-x-0 h-fit bg-zinc-100 border-b border-zinc-300 z-[10] py-2'>
 			<div className='container max-w-7xl h-full mx-auto flex items-center justify-between gap-2'>
@@ -23,16 +23,14 @@ export default function Navbar() {
 				{/* <SearchBar /> */}
 
 				{/* actions */}
-				{/* {session?.user ? (
-					<UserAccountNav user={session.user} />
+				{session?.user ? (
+					// <UserAccountNav user={session.user} />
+					<p>Nikolay</p>
 				) : (
 					<Link href='/sign-in' className={buttonVariants()}>
 						Sign In
 					</Link>
-				)} */}
-				<Link href='/sign-in' className={buttonVariants()}>
-					Sign In
-				</Link>
+				)}
 			</div>
 		</div>
 	)
---
+++ src/components/UserAuthForm.tsx
@@ -0,0 +1,49 @@
+'use client'
+
+import { cn } from '@/lib/utils'
+import { signIn } from 'next-auth/react'
+import { Button } from '@/components/ui/Button'
+import { useToast } from '@/hooks/use-toast'
+import { Icons } from './Icons'
+import { useState } from 'react'
+
+interface UserAuthFormProps extends React.HTMLAttributes<HTMLDivElement> {}
+
+export default function UserAuthForm({
+	className,
+	...props
+}: UserAuthFormProps) {
+	const { toast } = useToast()
+	const [isLoading, setIsLoading] = useState<boolean>(false)
+
+	async function loginWithGoogle() {
+		setIsLoading(true)
+		try {
+			await signIn('google')
+		} catch (error) {
+			toast({
+				title: 'Error',
+				description: 'There was an error logging in with Google',
+				variant: 'destructive'
+			})
+		} finally {
+			setIsLoading(false)
+		}
+	}
+
+	return (
+		<div className={cn('flex justify-center', className)} {...props}>
+			<Button
+				isLoading={isLoading}
+				type='button'
+				size='sm'
+				className='w-full'
+				onClick={loginWithGoogle}
+				disabled={isLoading}
+			>
+				{isLoading ? null : <Icons.google className='h-4 w-4 mr-2' />}
+				Google
+			</Button>
+		</div>
+	)
+}
