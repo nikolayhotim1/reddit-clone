f273a41fd72801179d32d7ebf88c17e39e08f8f6
nikolayhotim1 <hotim_nikolay@mail.ru>
2023-07-06 23:32:49 +0300

    feature: create subreddits

---
+++ src/components/Providers.tsx
@@ -0,0 +1,19 @@
+'use client'
+
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query'
+import { SessionProvider } from 'next-auth/react'
+import { ReactNode } from 'react'
+
+const queryClient = new QueryClient()
+
+interface LayoutProps {
+	children: ReactNode
+}
+
+export default function Providers({ children }: LayoutProps) {
+	return (
+		<QueryClientProvider client={queryClient}>
+			<SessionProvider>{children}</SessionProvider>
+		</QueryClientProvider>
+	)
+}
