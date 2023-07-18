bd26ca7650bd43fd446d1f56ca07cd8a33c680b1
nikolayhotim1 <hotim_nikolay@mail.ru>
2023-07-19 00:37:48 +0300

    feature: posts voting, optimizing post performance

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -6,7 +6,7 @@ import { MessageSquare } from 'lucide-react'
 import Link from 'next/link'
 import { useRef } from 'react'
 import { EditorOutput } from './EditorOutput'
-// import { PostVoteClient } from './post-vote/PostVoteClient'
+import { PostVoteClient } from './post-vote/PostVoteClient'
 
 type PartialVote = Pick<Vote, 'type'>
 interface PostProps {
@@ -22,8 +22,8 @@ interface PostProps {
 
 export function Post({
 	post,
-	// votesAmt: _votesAmt,
-	// currentVote: _currentVote,
+	votesAmt: votesAmt,
+	currentVote: currentVote,
 	subredditName,
 	commentAmt
 }: PostProps) {
@@ -31,11 +31,11 @@ export function Post({
 	return (
 		<div className='rounded-md bg-white shadow'>
 			<div className='px-6 py-4 flex justify-between'>
-				{/* <PostVoteClient
+				<PostVoteClient
 					postId={post.id}
-					initialVotesAmt={_votesAmt}
-					initialVote={_currentVote?.type}
-				/> */}
+					initialVotesAmt={votesAmt}
+					initialVote={currentVote?.type}
+				/>
 				<div className='w-0 flex-1'>
 					<div className='max-h-40 mt-1 text-xs text-gray-500'>
 						{subredditName ? (
