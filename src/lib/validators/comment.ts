e15509948ed043b38c47dfb46c208b548ba0aefd
nikolayhotim1 <hotim_nikolay@mail.ru>
2023-07-23 12:24:59 +0300

    feature: post comments, replying & voiting

---
+++ src/lib/validators/comment.ts
@@ -0,0 +1,8 @@
+import { z } from 'zod'
+
+export const CommentValidator = z.object({
+	postId: z.string(),
+	text: z.string(),
+	replyToId: z.string().optional()
+})
+export type CommentRequest = z.infer<typeof CommentValidator>
